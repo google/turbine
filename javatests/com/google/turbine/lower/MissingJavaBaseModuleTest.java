5f2485b202ab6c1d471c08b40a6b9332b0183a12
Liam Miller-Cushon <cushon@google.com>
2023-09-19 08:43:46 -0700

    Rename a test that wasn't being run
    PiperOrigin-RevId: 566644205

--- javatests/com/google/turbine/lower/MissingJavaBaseModuleTest.java
+++ javatests/com/google/turbine/lower/MissingJavaBaseModuleTest.java
@@ -40,7 +40,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class MissingJavaBaseModule {
+public class MissingJavaBaseModuleTest {
 
   @Test
   public void test() throws Exception {
